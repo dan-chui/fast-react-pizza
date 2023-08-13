bd4482b8be91615b87b43d6e84278638d599ccd8
Dan Chui <dchui108@gmail.com>
2023-08-13 22:43:57 +0900

    edit Cart features

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { updateName } from "./userSlice";
+import { useNavigate } from "react-router-dom";
 
 function CreateUser() {
   const [username, setUsername] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!username) return;
+    dispatch(updateName(username));
+    navigate("/menu");
   }
 
   return (
@@ -16,10 +25,10 @@ function CreateUser() {
 
       <input
         type="text"
-        placeholder="Your full name"
+        placeholder="Your Full Name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
-        className="input mb-8 w-72"
+        className="input mb-8 w-72 text-center"
       />
 
       {username !== "" && (
