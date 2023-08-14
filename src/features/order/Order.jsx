a7150493931948b3feb2913f0998dfeb770253c2
Dan Chui <dchui108@gmail.com>
2023-08-14 23:09:35 +0900

    edit New Orders

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -54,17 +54,17 @@ function Order() {
 
       <ul className="dive-stone-200 divide-y border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.id} />
+          <OrderItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
       <div className="space-y-2 bg-stone-200 px-6 py-5">
         <p className="text-sm font-medium text-stone-600">
-          Price pizza: {formatCurrency(orderPrice)}
+          Price (Pizza): {formatCurrency(orderPrice)}
         </p>
         {priority && (
           <p className="text-sm font-medium text-stone-600">
-            Price priority: {formatCurrency(priorityPrice)}
+            Price (Priority): {formatCurrency(priorityPrice)}
           </p>
         )}
         <p className="font-bold">
